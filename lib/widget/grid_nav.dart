0d85afac8a78761b8ffffe2c9266eb47256491a2
yongkang <40753890+LostKK@users.noreply.github.com>
2021-05-25 18:15:45 +0800

    11.12 7:51

--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -26,9 +26,86 @@ class GridNav extends StatelessWidget {
   _gridNavItemsSingle(
       BuildContext context, GridNavItem gridNavItem, bool first) {
     List<Widget> items = [];
+    items.add(_mainItem(context, gridNavItem.mainItem));
+    items.add(_doubleItem(context, gridNavItem.item1, gridNavItem.item2));
+    items.add(_doubleItem(context, gridNavItem.item3, gridNavItem.item4));
+    List<Widget> expandItems = [];
+    items.forEach((item) {
+      expandItems.add(Expanded(child: item, flex: 1));
+    });
+    Color startColor = Color(int.parse('0xff' + gridNavItem.startColor));
+    Color endColor = Color(int.parse('0xff' + gridNavItem.endColor));
+    return Container(
+      height: 88,
+      margin: first ? null : EdgeInsets.only(top: 3),
+      decoration: BoxDecoration(
+          //线性渐变
+          gradient: LinearGradient(colors: [startColor, endColor])),
+      // TODO
+    );
   }
 
   _mainItem(BuildContext context, CommonModel model) {
+    return _wrapGesture(
+        context,
+        Stack(
+          children: <Widget>[
+            Image.network(
+              model.icon,
+              fit: BoxFit.contain,
+              height: 88,
+              width: 121,
+              alignment: AlignmentDirectional.bottomEnd,
+            ),
+            Text(
+              model.title,
+              style: TextStyle(fontSize: 14, color: Colors.white),
+            )
+          ],
+        ),
+        model);
+  }
+
+  _doubleItem(
+      BuildContext context, CommonModel topItem, CommonModel bottomItem) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: _item(context, topItem, true),
+        ),
+        Expanded(
+          child: _item(context, bottomItem, false),
+        )
+      ],
+    );
+  }
+
+  _item(BuildContext context, CommonModel item, bool first) {
+    BorderSide borderSide = BorderSide(width: 0.8, color: Colors.white);
+
+    return FractionallySizedBox(
+      //撑满父布局的宽度
+      widthFactor: 1,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+          left: borderSide,
+          bottom: first ? borderSide : BorderSide.none,
+        )),
+        child: Center(
+            child: _wrapGesture(
+                context,
+                Text(
+                  item.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                item)),
+      ),
+    );
+  }
+
+  _wrapGesture(BuildContext context, Widget widget, CommonModel model) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -41,21 +118,6 @@ class GridNav extends StatelessWidget {
                       hideAppBar: model.hideAppBar,
                     )));
       },
-      child: Stack(
-        children: <Widget>[
-          Image.network(
-            model.icon,
-            fit: BoxFit.contain,
-            height: 88,
-            width: 121,
-            alignment: AlignmentDirectional.bottomEnd,
-          ),
-          Text(
-            model.title,
-            style: TextStyle(fontSize: 14, color: Colors.white),
-          )
-        ],
-      ),
     );
   }
 }
