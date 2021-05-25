248a7fcd52f73a2137330eccac7fcead5858b852
yongkang <40753890+LostKK@users.noreply.github.com>
2021-05-25 16:30:49 +0800

    11.x

--- lib/widget/webview.dart
+++ lib/widget/webview.dart
@@ -4,6 +4,8 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
+const CATCH_URLS = ['m.ctrip.com/', 'm.ctrip.com/html5/', 'm.ctrip.com/html5'];
+
 class WebView extends StatefulWidget {
   final String url;
   final String statusBarColor;
@@ -17,7 +19,7 @@ class WebView extends StatefulWidget {
       this.statusBarColor,
       this.title,
       this.hideAppBar,
-      this.backForbid})
+      this.backForbid = false})
       : super(key: key);
 
   @override
@@ -29,15 +31,25 @@ class _WebViewState extends State<WebView> {
   StreamSubscription<String> _onUrlChange;
   StreamSubscription<WebViewStateChanged> _onStateChanged;
   StreamSubscription<WebViewHttpError> _onHttpError;
+  bool exiting = false;
 
   @override
   void initState() {
     super.initState();
     webviewReference.close();
     _onUrlChange = webviewReference.onUrlChanged.listen((String url) {});
-    webviewReference.onStateChanged.listen((WebViewStateChanged state) {
+    _onStateChanged =
+        webviewReference.onStateChanged.listen((WebViewStateChanged state) {
       switch (state.type) {
         case WebViewState.startLoad:
+          if (_isToMain(state.url) && !exiting) {
+            if (widget.backForbid) {
+              webviewReference.launch(widget.url);
+            } else {
+              Navigator.pop(context);
+              exiting = true;
+            }
+          }
           break;
         default:
           break;
@@ -47,6 +59,17 @@ class _WebViewState extends State<WebView> {
         webviewReference.onHttpError.listen((WebViewHttpError error) {
       print(error);
     });
+  }
+
+  _isToMain(String url) {
+    bool contain = false;
+    for (final value in CATCH_URLS) {
+      if (url?.endsWith(value) ?? false) {
+        contain = true;
+        break;
+      }
+    }
+    return contain;
   }
 
   @override
@@ -60,10 +83,33 @@ class _WebViewState extends State<WebView> {
 
   @override
   Widget build(BuildContext context) {
+    String statusBarColorStr = widget.statusBarColor ?? 'ffffff';
+    Color backButtonColor;
+    if (statusBarColorStr == 'ffffff') {
+      backButtonColor = Colors.black;
+    } else {
+      backButtonColor = Colors.white;
+    }
     return Scaffold(
       body: Column(
         children: <Widget>[
-          _appBar(null, null),
+          _appBar(
+              Color(
+                int.parse('0xff' + statusBarColorStr),
+              ),
+              backButtonColor),
+          Expanded(
+              child: WebviewScaffold(
+            url: widget.url,
+            withZoom: true,
+            withLocalStorage: true,
+            initialChild: Container(
+              color: Colors.white,
+              child: Center(
+                child: Text('waiting...'),
+              ),
+            ),
+          ))
         ],
       ),
     );
